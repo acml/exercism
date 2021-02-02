4d5d0798949faa807cd5b9111474f76035992d8e
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2021-02-02 18:19:59 +0300

    Calculate Total without a for loop

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -13,10 +13,12 @@ func Square(n int) (uint64, error) {
 }
 
 // Total gives the total number of grains on the chessboard.
+// Total progression:
+// 1 (1 = 2 * (1 << (1 - 1)) - 1) -> 2 * Square(1) - 1
+// 3 (3 = 2 * (1 << (2 - 1)) - 1) -> 2 * Square(2) - 1
+// 7 (7 = 2 * (1 << (3 - 1)) - 1) -> 2 * Square(3) - 1
+// ...
+// x (x = 2 * (1 << (n - 1)) - 1)) -> 2 * Square(n) - 1
 func Total() uint64 {
-	var sum uint64
-	for n := 1; n <= 64; n++ {
-		sum += (1 << (n - 1))
-	}
-	return sum
+	return 2*(1<<(64-1)) - 1
 }
