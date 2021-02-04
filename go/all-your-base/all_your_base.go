eaae4bb972131b48a6b784cfb7e263b1edf93eb6
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2021-02-04 19:06:51 +0300

    completed all-your-base, allergies, phone-number and sieve exercises

---
+++ go/all-your-base/all_your_base.go
@@ -0,0 +1,36 @@
+// Package allyourbase provides a base convertion function
+package allyourbase
+
+import "errors"
+
+// ConvertToBase converts a number, represented as a sequence of digits in one
+// base, to any other base.
+func ConvertToBase(inputBase int, inputDigits []int, outputBase int) ([]int, error) {
+	switch {
+	case inputBase < 2:
+		return []int{}, errors.New("input base must be >= 2")
+	case outputBase < 2:
+		return []int{}, errors.New("output base must be >= 2")
+	}
+
+	var val int
+	for i, pow := 0, 1; i < len(inputDigits); i++ {
+		if inputDigits[i] < 0 || inputDigits[i] >= inputBase {
+			return []int{}, errors.New("all digits must satisfy 0 <= d < input base")
+		}
+		val += pow * inputDigits[len(inputDigits)-i-1]
+		pow = pow * inputBase
+	}
+
+	output := []int{}
+	for pow := 1; ; {
+		digitVal := val % (pow * outputBase)
+		output = append(append([]int{}, digitVal/pow), output...)
+		val -= digitVal
+		pow = pow * outputBase
+		if val == 0 {
+			break
+		}
+	}
+	return output, nil
+}
---
+++ go/phone-number/phone_number.go
@@ -0,0 +1,43 @@
+// Package phonenumber provides functions to clean up user-entered phone numbers
+// so that they can be sent SMS messages.
+package phonenumber
+
+import (
+	"errors"
+	"regexp"
+)
+
+// Number validates the input phone number.
+func Number(input string) (string, error) {
+	var re = regexp.MustCompile(`(?:\+)?(1)?(?:\s*)\(?([2-9]\d{2})\)?(?:\s*|[-.])([2-9]\d{2})(?:\s*|[-.])(\d{4})(?:\s*)`)
+	if !re.MatchString(input) {
+		return "", errors.New("No match")
+	}
+
+	if res := re.ReplaceAllString(input, "$1$2$3$4"); len(res) == 11 {
+		if res[0] != '1' {
+			return "", errors.New("11 digits does not start with a 1")
+		}
+	} else if len(res) > 11 {
+		return "", errors.New("invalid when more than 11 digits")
+	}
+	return re.ReplaceAllString(input, "$2$3$4"), nil
+}
+
+// AreaCode prints the area code part of the phone number.
+func AreaCode(input string) (string, error) {
+	number, err := Number(input)
+	if err != nil {
+		return "", err
+	}
+	return number[:3], nil
+}
+
+// Format outputs phone numbers in a standart format.
+func Format(input string) (string, error) {
+	number, err := Number(input)
+	if err != nil {
+		return "", err
+	}
+	return "(" + number[:3] + ") " + number[3:6] + "-" + number[6:], nil
+}
