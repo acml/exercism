5f57ff30ee297d29abb71a80f209c7dbba88a343
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2021-02-15 01:33:52 +0300

    made changes according to mentor comments

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -11,12 +11,12 @@ Distance calculates the Hamming Distance between two DNA sequences of equal
 length.
 */
 func Distance(a, b string) (int, error) {
-	hammingDistance := 0
 
 	if len(a) != len(b) {
 		return 0, errors.New("sequences must be of equal length")
 	}
 
+	var hammingDistance int
 	for i := 0; i < len(a); i++ {
 		if a[i] != b[i] {
 			hammingDistance++
