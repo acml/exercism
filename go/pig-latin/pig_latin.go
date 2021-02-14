81fcc66beef72edcb03c098466697a57abd6b22d
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2021-02-14 03:17:05 +0300

    submitted pig-latin exercise

---
+++ go/pig-latin/pig_latin.go
@@ -0,0 +1,24 @@
+package piglatin
+
+import "strings"
+
+// Sentence converts input strings to Pig Latin which is a made-up children's
+// language that's intended to be confusing.
+func Sentence(input string) string {
+	inputs := strings.Fields(input)
+
+	res := []string{}
+	for _, input := range inputs {
+		if strings.HasPrefix(input, "xr") || strings.HasPrefix(input, "yt") || strings.IndexAny(input, "aeiou") == 0 {
+			res = append(res, input+"ay")
+		} else {
+			index := strings.IndexFunc(input, func(r rune) bool { return strings.ContainsRune("aeio", r) })
+			if index < 0 {
+				index = strings.IndexFunc(input, func(r rune) bool { return strings.ContainsRune("uy", r) })
+			}
+			res = append(res, input[index:]+input[:index]+"ay")
+		}
+	}
+	return strings.Join(res, " ")
+
+}
