f00ba09c167f768784222d324494236faf9efaa6
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2021-02-16 01:35:05 +0300

    renamed variable, commented exported variable

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -1,8 +1,10 @@
 package space
 
+// Planet will be the name of the planet which we will be making calculations
+// for.
 type Planet string
 
-var earth_year = map[Planet]float64{
+var earthYear = map[Planet]float64{
 	"Mercury": 0.2408467,
 	"Venus":   0.61519726,
 	"Earth":   1.0,
@@ -13,7 +15,9 @@ var earth_year = map[Planet]float64{
 	"Neptune": 164.79132,
 }
 
+// Age given an age in seconds, calculates how old someone would be on other
+// planets.
 func Age(seconds float64, planet Planet) float64 {
 
-	return seconds / (31557600.0 * earth_year[planet])
+	return seconds / (31557600.0 * earthYear[planet])
 }
