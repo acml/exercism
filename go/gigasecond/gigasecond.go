75bf9e283bd841aa67f709655c803a3c412188eb
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2021-02-16 01:15:25 +0300

    fixed comment style

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -3,8 +3,8 @@ package gigasecond
 // import path for the time package from the standard library
 import "time"
 
-// Given a moment, determine the moment that would be after a gigasecond
-// has passed.
+// AddGigasecond given a moment, determines the moment that would be after a
+// gigasecond has passed.
 func AddGigasecond(t time.Time) time.Time {
 	return t.Add(1e9 * time.Second)
 }
