4dfa0eba062e5727766a5b613bb355747b84a06b
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2021-02-16 01:18:48 +0300

    add comment to exported function

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -2,6 +2,11 @@ package collatzconjecture
 
 import "errors"
 
+// CollatzConjecture performs The Collatz Conjecture
+// Take any positive integer n. If n is even, divide n by 2 to get n / 2. If n is
+// odd, multiply n by 3 and add 1 to get 3n + 1. Repeat the process indefinitely.
+// The conjecture states that no matter which number you start with, you will
+// always reach 1 eventually.
 func CollatzConjecture(n int) (int, error) {
 
 	if n <= 0 {
