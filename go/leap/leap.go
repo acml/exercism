aefac9685c03f61c7742478286da68f031fda945
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2021-02-16 01:12:33 +0300

    fixed comment style, simplified return value calculation

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,17 +1,13 @@
 package leap
 
-// Given a year, report if it is a leap year.
+// IsLeapYear given a year, reports if it is a leap year.
 func IsLeapYear(year int) bool {
 	if year%4 != 0 {
 		return false
 	}
 
 	if year%100 == 0 {
-		if year%400 == 0 {
-			return true
-		}
-
-		return false
+		return year%400 == 0
 	}
 
 	return true
