356bf0a9ece7522877e7b4836758aacad4be9d11
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2021-03-02 00:45:32 +0300

    refactor

--- go/ledger/ledger.go
+++ go/ledger/ledger.go
@@ -4,42 +4,53 @@ import (
 	"errors"
 	"fmt"
 	"sort"
+	"strconv"
 	"strings"
 	"time"
 )
 
-type numbers struct {
-	decimalSeperator string
-	digitGrouping    string
-	negativeStart    string
-	negativeEnd      string
-	symbolSeparator  string
-}
+type (
+	// Entry is a typical ledger input entry
+	Entry struct {
+		Date        string // "Y-m-d"
+		Description string
+		Change      int // in cents
+	}
 
-type literals struct {
-	date        string
-	description string
-	change      string
-	dateFormat  string
-	number      *numbers
-}
+	numbers struct {
+		decimalSeperator string
+		digitGrouping    string
+		negativeStart    string
+		negativeEnd      string
+		symbolSeparator  string
+	}
 
-var langs = map[string]*literals{
-	"en-US": {"Date", "Description", "Change", "01/02/2006", &numbers{".", ",", "(", ")", ""}},
-	"nl-NL": {"Datum", "Omschrijving", "Verandering", "02-01-2006", &numbers{",", ".", "", "-", " "}},
-}
+	literals struct {
+		date        string
+		description string
+		change      string
+		dateFormat  string
+		number      *numbers
+	}
+)
 
-var symbols = map[string]rune{
-	"EUR": '€',
-	"USD": '$',
-}
+const (
+	dateLayout = "2006-01-02"
+	headerFormat = "%-10s | %-25s | %s\n"
+	entryFormat = "%-10s | %-25s | %13s\n"
+)
 
-// Entry is a typical ledger input entry
-type Entry struct {
-	Date        string // "Y-m-d"
-	Description string
-	Change      int // in cents
-}
+var (
+	langs = map[string]*literals{
+		"en-US": {"Date", "Description", "Change", "01/02/2006", &numbers{".", ",", "(", ")", ""}},
+		"nl-NL": {"Datum", "Omschrijving", "Verandering", "02-01-2006", &numbers{",", ".", "", "-", " "}},
+	}
+
+	symbols = map[string]rune{
+		"EUR": '€',
+		"USD": '$',
+	}
+)
 
 func formatChange(cents int, number *numbers, symbol rune) string {
 	a, negative := "", false
@@ -49,7 +60,7 @@ func formatChange(cents int, number *numbers, symbol rune) string {
 		a += number.negativeStart
 	}
 
-	wholeStr := fmt.Sprintf("%d", cents/100)
+	wholeStr := strconv.Itoa(cents/100)
 	for i := len(wholeStr) - 3; i > 0; i -= 3 {
 		wholeStr = wholeStr[:i] + number.digitGrouping + wholeStr[i:]
 	}
@@ -81,11 +92,12 @@ func FormatLedger(currency string, locale string, entries []Entry) (string, erro
 	}
 
 	if len(entries) == 0 {
-		return "Date       | Description               | Change\n", nil
+		l := langs["en-US"]
+		return fmt.Sprintf(headerFormat, l.date, l.description, l.change), nil
 	}
 
-	var entriesCopy []Entry
-	entriesCopy = append(entriesCopy, entries...)
+	entriesCopy := make([]Entry, len(entries))
+	copy(entriesCopy, entries)
 	sort.Slice(entriesCopy, func(i, j int) bool {
 		return entriesCopy[i].Date < entriesCopy[j].Date ||
 			entriesCopy[i].Description < entriesCopy[j].Description ||
@@ -100,7 +112,7 @@ func FormatLedger(currency string, locale string, entries []Entry) (string, erro
 	}, 10)
 	for i, et := range entriesCopy {
 		go func(i int, entry Entry) {
-			t, err := time.Parse("2006-01-02", entry.Date)
+			t, err := time.Parse(dateLayout, entry.Date)
 			if err != nil {
 				co <- struct {
 					i int
@@ -109,14 +121,11 @@ func FormatLedger(currency string, locale string, entries []Entry) (string, erro
 				}{e: err}
 				return
 			}
-			date := t.Format(lang.dateFormat)
 
 			desc := entry.Description
 			if len(desc) > 25 {
 				desc = desc[:22] + "..."
 			}
-
-			amount := formatChange(entry.Change, lang.number, symbol)
 
 			co <- struct {
 				i int
@@ -124,9 +133,7 @@ func FormatLedger(currency string, locale string, entries []Entry) (string, erro
 				e error
 			}{
 				i: i,
-				s: date + strings.Repeat(" ", 10-len(date)) + " | " +
-					desc + strings.Repeat(" ", 25-len(desc)) + " | " +
-					strings.Repeat(" ", 13-len([]rune(amount))) + amount + "\n",
+				s: fmt.Sprintf(entryFormat, t.Format(lang.dateFormat), desc, formatChange(entry.Change, lang.number, symbol)),
 			}
 		}(i, et)
 	}
@@ -139,8 +146,6 @@ func FormatLedger(currency string, locale string, entries []Entry) (string, erro
 		ss[v.i] = v.s
 	}
 
-	return lang.date + strings.Repeat(" ", 10-len(lang.date)) + " | " +
-		lang.description + strings.Repeat(" ", 25-len(lang.description)) + " | " +
-		lang.change + "\n" +
+	return fmt.Sprintf(headerFormat, lang.date, lang.description, lang.change) +
 		strings.Join(ss, ""), nil
 }
