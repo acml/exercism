b85f3ea1d27b7bc0da326b5d3fbe5928f0562366
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2021-03-09 02:06:35 +0300

    second iteration of robot_name

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -1,56 +1,65 @@
 package robotname
 
 import (
-	"fmt"
+	"errors"
 	"math/rand"
+	"strings"
+	"sync"
 	"time"
 )
 
 // Robot is identified with a name
 type Robot struct {
-	name *string
+	name string
 }
 
-var names []*string
-var namesInitialized = false
-var availableNames = 26 * 26 * 1000
+const maxRobotNames = 26 * 26 * 10 * 10 * 10
+
+var names = map[int]struct{}{}
+var availableNames = maxRobotNames
 
 // Name generates a random name for each robot in the format of two uppercase
 // letters followed by three digits, such as RX837 or BC811.
 func (r *Robot) Name() (string, error) {
-	if !namesInitialized {
-		i := 0
-		names = make([]*string, availableNames)
-		rand.Seed(time.Now().UnixNano())
-		for n1 := 'A'; n1 <= 'Z'; n1++ {
-			for n2 := 'A'; n2 <= 'Z'; n2++ {
-				for n3 := 0; n3 < 1000; n3++ {
-					temp := fmt.Sprintf("%c%c%03d", n1, n2, n3)
-					names[i] = &temp
-					i++
-				}
-			}
-		}
-		namesInitialized = true
-	}
-
-	if r.name == nil {
+	if r.name == "" {
 		if availableNames == 0 {
-			return "", fmt.Errorf("used all names")
+			return "", errors.New("used all names")
 		}
-		// select a random name
-		i := rand.Intn(availableNames)
-		r.name = names[i]
-
-		// delete the assigned name from the list
-		names[i] = names[availableNames-1] // Copy the last element to index i.
-		availableNames--
+		r.name = randRobotName()
 	}
-	return *r.name, nil
+	return r.name, nil
 }
 
 // Reset wipes the robot name and generates a new one.
 func (r *Robot) Reset() (string, error) {
-	r.name = nil
+	r.name = ""
 	return r.Name()
+}
+
+func randRobotName() string {
+
+	once := sync.Once{}
+	once.Do(func() { rand.Seed(time.Now().UnixNano()) })
+
+	var idx int
+	for {
+		idx = rand.Intn(maxRobotNames)
+		if _, ok := names[idx]; ok {
+			continue
+		}
+		break
+	}
+
+	names[idx] = struct{}{}
+	availableNames--
+
+	sb := strings.Builder{}
+	sb.Grow(5)
+
+	sb.WriteRune('A' + rune(idx/(26*1000)))
+	sb.WriteRune('A' + rune((idx%(26*1000))/1000))
+	sb.WriteRune('0' + rune((idx%1000)/100))
+	sb.WriteRune('0' + rune(((idx%1000)%100)/10))
+	sb.WriteRune('0' + rune((idx%1000)%10))
+	return sb.String()
 }
