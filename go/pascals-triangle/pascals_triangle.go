59711b09e3404bbb6eff81890c7dcc6d722606fe
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2021-03-14 23:45:41 +0300

    recursive solution of pascals_triangle exercise

--- go/pascals-triangle/pascals_triangle.go
+++ go/pascals-triangle/pascals_triangle.go
@@ -1,17 +1,18 @@
 package pascal
 
+// Triangle computes Pascal's triangle up to a given number of rows.
 func Triangle(n int) [][]int {
 	if n == 1 {
 		return [][]int{{1}}
-	} else if n == 2 {
-		return append(Triangle(1), []int{1, 1})
 	}
 
-	pres := Triangle(n - 1)
-	res := []int{1}
-	for i := 0; i < len(pres)-1; i++ {
-		res = append(res, pres[len(pres)-1][i]+pres[len(pres)-1][i+1])
+	triangle := Triangle(n - 1)
+	row := make([]int, n)
+	row[0] = 1
+	for i := 0; i < len(triangle)-1; i++ {
+		row[i+1] = triangle[len(triangle)-1][i] + triangle[len(triangle)-1][i+1]
 	}
-	res = append(res, 1)
-	return append(Triangle(n-1), res)
+	row[len(row)-1] = 1
+
+	return append(triangle, row)
 }
