0119fd562831aa13a7189ac52fb235dc15e8a6b8
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2021-03-15 00:43:10 +0300

    included recursive solution in comments

--- go/pascals-triangle/pascals_triangle.go
+++ go/pascals-triangle/pascals_triangle.go
@@ -19,3 +19,20 @@ func row(n int) []int {
 	}
 	return res
 }
+
+// recursive solution
+// func Triangle(n int) [][]int {
+// 	if n == 1 {
+// 		return [][]int{{1}}
+// 	}
+
+// 	triangle := Triangle(n - 1)
+// 	row := make([]int, n)
+// 	row[0] = 1
+// 	for i := 0; i < len(triangle)-1; i++ {
+// 		row[i+1] = triangle[len(triangle)-1][i] + triangle[len(triangle)-1][i+1]
+// 	}
+// 	row[len(row)-1] = 1
+
+// 	return append(triangle, row)
+// }
