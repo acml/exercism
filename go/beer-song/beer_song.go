081b2da208de03af2e1a5d52ba9e0e028022c14f
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2021-04-11 00:54:45 +0300

    second iteration of beer_song exercise

--- go/beer-song/beer_song.go
+++ go/beer-song/beer_song.go
@@ -3,7 +3,6 @@ package beer
 import (
 	"errors"
 	"fmt"
-	"strings"
 )
 
 // Verse returns a specific verse of the beer song.
@@ -11,27 +10,24 @@ func Verse(n int) (string, error) {
 	if n > 99 {
 		return "", errors.New("invalind verse number")
 	}
-	sb := strings.Builder{}
-	switch {
-	case n > 1:
-		fmt.Fprintf(&sb, "%d bottles of beer on the wall, %d bottles of beer.\n", n, n)
-	case n == 1:
-		sb.WriteString("1 bottle of beer on the wall, 1 bottle of beer.\n")
-	case n == 0:
-		sb.WriteString("No more bottles of beer on the wall, no more bottles of beer.\n")
-	}
-
 	switch {
 	case n > 2:
-		fmt.Fprintf(&sb, "Take one down and pass it around, %d bottles of beer on the wall.\n", n-1)
+		return fmt.Sprintf(`%d bottles of beer on the wall, %d bottles of beer.
+Take one down and pass it around, %d bottles of beer on the wall.
+`, n, n, n-1), nil
 	case n == 2:
-		sb.WriteString("Take one down and pass it around, 1 bottle of beer on the wall.\n")
+		return `2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+`, nil
 	case n == 1:
-		sb.WriteString("Take it down and pass it around, no more bottles of beer on the wall.\n")
-	case n == 0:
-		sb.WriteString("Go to the store and buy some more, 99 bottles of beer on the wall.\n")
+		return `1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.
+`, nil
+	default:
+		return `No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+`, nil
 	}
-	return sb.String(), nil
 }
 
 // Verses returns verses of a given range.
@@ -39,15 +35,15 @@ func Verses(upperBound, lowerBound int) (string, error) {
 	if lowerBound < 0 || upperBound > 99 || lowerBound > upperBound {
 		return "", errors.New("invalid range")
 	}
-	sb := strings.Builder{}
+	verses := ""
 	for i := upperBound; i >= lowerBound; i-- {
 		v, err := Verse(i)
 		if err != nil {
 			return "", errors.New("erroneous verse")
 		}
-		fmt.Fprintf(&sb, "%s\n", v)
+		verses += v + "\n"
 	}
-	return sb.String(), nil
+	return verses, nil
 }
 
 // Song recites the full beer song.
